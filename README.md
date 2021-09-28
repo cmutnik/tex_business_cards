794a65d426d1a8adcbf69929725cdf828ebc4164
Corey Mutnik <cmutnik@hawaii.edu>
2021-09-27 15:01:48 -1000

    updated formatting

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# tex_business_cards
-Make Business Cards in Latex
+# Simple Business Cards #
 
-Use [latex template](./data_scientist_simple_design_logos.tex) to generate PDF.  Invert PDF colors using python script, [`invert_images.py`](./invert_images.py), to invert the colors:
+Use Latex and Python to make simple business cards
+
+Use [latex template](./data_scientist_simple_design_logos.tex) to generate a PDF.  The python script, [`invert_images.py`](./invert_images.py), is used to invert the PDF colors and crop to the standard dimensions.
 
 ![](./figs/inverted_business_card.jpg)
--- invert_images.py
+++ invert_images.py
@@ -1,15 +1,22 @@
+from pdf2image import convert_from_path
 from PIL import Image
 import numpy as np
-from pdf2image import convert_from_path
 
 _in_file_ = "./figs/data_scientist_simple_design_logos.pdf"
-_out_file_ = "./figs/inverted_business_card.jpg"
+_out_file_ = "./figs/inverted_business_card"
 
 images = convert_from_path(_in_file_)
 img = np.array(images[0])
-# img = Image.open(r"test.jpg")
-img_arry = np.array(img)
-I_max = 255
-img_arry = I_max - img_arry
-inverted_img = Image.fromarray(img_arry)
-inverted_img.save(_out_file_)
+img_array = np.array(img)
+I_max = 255 # img_array.max()
+img_array = I_max - img_array
+x, y = 0, img_array.shape[0]
+h, w = 633, 1083
+crop = img_array[y-h:y, x:x+w]
+
+inverted_img_cropped = Image.fromarray(crop)
+inverted_img_cropped.save(_out_file_ + ".jpg")
+
+img1 = Image.open(_out_file_ + ".jpg")
+im1 = img1.convert("RGB")
+im1.save(_out_file_ + ".pdf")
